1a6b6da00bc94130bc77ba347c38f3ec3d3ebe00
Graham Brooks <graham@grahambrooks.com>
2017-06-08 22:36:21 -0600

    Added tags so that code snippets can be pulled into blog

--- test/functional_event_store/core_test.clj
+++ test/functional_event_store/core_test.clj
@@ -9,7 +9,7 @@
        (fact (new-customer-email-event "a@b.com") => {:event-type :new-customer-email
                                                       :email      "a@b.com" }))
 
-
+; tag::new-customer-event[]
 (facts "about new customer events"
        (fact "customers can have just a name"
              (new-customer-event "fred") => {:event-type :new-customer
@@ -19,6 +19,7 @@
              (new-customer-event "fred" "a@b") => {:event-type :new-customer
                                                    :name       "fred"
                                                    :email      "a@b"}))
+; end::new-customer-event[]
 
 (facts "about handlers"
        (fact "Updates defined entity fields"
@@ -27,7 +28,7 @@
              (handle-event {:name "foo"} (new-customer-email-event "a@b") handler-map) => {:name "foo"})
        (fact "leaves empty entity unchanged"
              (handle-event {} (new-customer-email-event "a@b") handler-map) => {}))
-
+; tag::hydrate-entity[]
 (facts "about building entities"
        (fact "entity state represents the latest event states"
              (let [events [(new-customer-event "graham")
@@ -36,7 +37,7 @@
                            (new-customer-email-event "b@c")]]
                (hydrate-entity {:name nil :email nil} events) => {:name  "Graham"
                                                                   :email "b@c"})))
-
+; end::hydrate-entity[]
 (facts "about event store"
        (fact "can add event to empty store"
              (add-event :event) => [:event])
@@ -53,12 +54,15 @@
                                               :event-type :new-customer})
        
        (fact "event does not add fields"
+             ; tag::sparse-aggregate[]
              (let [aggregate (defaggregate {:name nil})]
                (handle-new-customer-event aggregate (new-customer-event "Graham Brooks" "graham@grahambrooks.com")) => {:name "Graham Brooks"}))
-       
+                                        ; end::sparse-aggregate[]
+       ; tag::full-aggregate[]
        (fact "event changes all event fields"
              (let [aggregate (defaggregate {:name nil :email nil})]
                (handle-new-customer-event aggregate (new-customer-event "Graham Brooks" "graham@grahambrooks.com")) => {:name "Graham Brooks" :email "graham@grahambrooks.com"}))
+                                        ; end::full-aggregate[]
        
        (fact
         (let [customer {:name "foo" :email "bar"}]
